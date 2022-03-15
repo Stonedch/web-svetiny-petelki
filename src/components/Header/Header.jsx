52903488b4f55b91e2b6fc8f5ad2c51660050404
Maxim Mukhin <stch.max.muhin@gmail.com>
2022-03-15 22:47:19 +0300

    feat: add Header block

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import styles from './Header.module.scss';
+import image from '../../assets/images/header-image.png';
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            <div className={styles.content}>
+                <div className={styles.wrapper}>
+                    <div className={styles.image}><img src={image} /></div>
+                </div>
+                <div className={styles.wrapper}>
+                    <h1>Lorem Ipsum</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export { Header };
+
