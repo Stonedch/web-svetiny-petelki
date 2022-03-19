88c0e9046fc8561ad1ca0afb17e67fea4e9c476f
Maxim Mukhin <stch.max.muhin@gmail.com>
2022-03-19 16:55:37 +0300

    chore: minor changes to settings

--- backend/web_svetpet/settings.py
+++ backend/web_svetpet/settings.py
@@ -99,6 +99,8 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'UPDATE_LAST_LOGIN': True,
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
 }
 
 ROOT_URLCONF = 'web_svetpet.urls'
