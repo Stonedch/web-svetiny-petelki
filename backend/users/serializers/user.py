6190845422ea829669caccd1efc59a80b33066de
Maxim Mukhin <stch.max.muhin@gmail.com>
2022-03-19 16:09:19 +0300

    chore: add "is_staff" to user serializer

--- backend/users/serializers/user.py
+++ backend/users/serializers/user.py
@@ -7,6 +7,6 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'is_active', 'created', 'updated']
+        fields = ['id', 'username', 'email', 'is_active', 'is_staff', 'created', 'updated']
         read_only_field = ['is_active', 'created', 'updated']
 
