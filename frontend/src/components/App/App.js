1b496db2a6284157758964786b3fc0fd154b3a2b
Maxim Mukhin <stch.max.muhin@gmail.com>
2022-03-20 19:00:59 +0300

    feat: add Category component

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -6,6 +6,7 @@ import { Button } from '../Button';
 import { Header } from '../Header';
 import { Navbar } from '../Navbar';
 import { Annonce } from '../Annonce';
+import { Category } from '../Category';
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
                 body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
                 picture={image}
             />
+            <Category title="Именные игрушки" picture={image} />
             <Annonce
                 title="Lorem Ipsum"
                 subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
