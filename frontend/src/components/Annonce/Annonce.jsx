2d69b0c2eb991d2de0716037837ae49f604a97e9
Maxim Mukhin <stch.max.muhin@gmail.com>
2022-03-20 16:46:08 +0300

    feat: add Annonce component

---
+++ frontend/src/components/Annonce/Annonce.jsx
@@ -0,0 +1,25 @@
+import styles from './Annonce.module.scss';
+
+const Annonce = (props) => {
+    const {title, subtitle, body, picture, reverse=false} = props
+
+    return (
+        <div className={styles.annonce}>
+            <div className={reverse ? [styles.content, styles.content_reverse].join(' ') : styles.content}>
+                <div className={styles.annonce__column}>
+                    <h2 className={styles.annonce__title}>{title}</h2>
+                    <h3 className={styles.annonce__subtitle}>{subtitle}</h3>
+                    <p className={styles.annonce__par}>{body}</p>
+                </div>
+                <div className={styles.annonce__column}>
+                    <div className={styles.annonce__picture}>
+                        <img src={picture} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export { Annonce };
+
--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,9 +1,11 @@
 import logo from '../../logo.svg';
 import './App.css';
 
+import image from '../../assets/images/header-image.png';
 import { Button } from '../Button';
 import { Header } from '../Header';
 import { Navbar } from '../Navbar';
+import { Annonce } from '../Annonce';
 
 function App() {
 
@@ -11,6 +13,19 @@ function App() {
         <div className="App">
             <Navbar />
             <Header />
+            <Annonce
+                title="Lorem Ipsum"
+                subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                picture={image}
+            />
+            <Annonce
+                title="Lorem Ipsum"
+                subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                reverse={true}
+                picture={image}
+            />
         </div>
     );
 }
