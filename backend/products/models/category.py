4f3e70298878e0f39e0f9a4b23fc450ba762001c
Maxim Mukhin <stch.max.muhin@gmail.com>
2022-03-23 11:42:54 +0300

    fix: rename picture field of category model

--- backend/products/models/category.py
+++ backend/products/models/category.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(db_index=True, max_length=255, unique=True)
-    picutre = models.ImageField(upload_to='categories')
+    picture = models.ImageField(upload_to='categories')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
