890c081e146cdcabc6e8a65d72c8e3fa7fa213d0
Maxim Mukhin <stch.max.muhin@gmail.com>
2022-03-23 12:09:10 +0300

    fix: media files

--- backend/web_svetpet/urls.py
+++ backend/web_svetpet/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rest_framework import routers
 
@@ -19,4 +21,6 @@ urlpatterns = [
         ]), name='v1')
     ]), name='api'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
