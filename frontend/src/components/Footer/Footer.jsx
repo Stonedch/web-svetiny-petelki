ffbeb4950e1b51b25869fa5225b9db417b1c891f
Maxim Mukhin <stch.max.muhin@gmail.com>
2022-03-29 11:30:46 +0300

    chore: minor changes to footer component

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import styles from './Footer.module.scss';
-import React from "react";
+import React from 'react';
 import facebook from '../../assets/images/Facebook.svg';
 import instag from '../../assets/images/Instagram.svg';
 import whatsapp from '../../assets/images/WhatsApp.svg';
@@ -7,23 +7,23 @@ import whatsapp from '../../assets/images/WhatsApp.svg';
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <div className={styles.massageBox}>
-        <a href="#"><img src={facebook} /></a>
-        <a href="#"><img src={instag} /></a>
-        <a href="#"><img src={whatsapp} /></a>
-      </div>
-      <div className={styles.lineBox}>
-        <div className={styles.line}></div>
-      </div>
-      <div className={styles.menu}>
-        <a href="#">Главная</a>
-        <a href="#">Наборы</a>
-        <a href="#">Именные игрушки</a>
-        <a href="#">Контакты</a>
-      </div>
-      <div className={styles.сopyright}>
-        <a> ©2022 Svetenpetelki. All rights reserved </a>
-      </div>
+        <div className={styles.content}>
+            <div className={styles.massageBox}>
+                <a href='#'><img src={facebook} /></a>
+                <a href='#'><img src={instag} /></a>
+                <a href='#'><img src={whatsapp} /></a>
+            </div>
+            <div className={styles.lineBox}>
+                <div className={styles.line}></div>
+            </div>
+            <div className={styles.menu}>
+                <a href='/'>Главная</a>
+                <a href='#'>Наборы</a>
+                <a href='#'>Именные игрушки</a>
+                <a href='#'>Контакты</a>
+            </div>
+            <a href='#' className={styles.сopyright}>©2022 Svetenpetelki. All rights reserved</a>
+        </div>
     </div>
   );
 };
