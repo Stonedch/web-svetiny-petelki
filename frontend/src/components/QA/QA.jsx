7e4873e059f1481f02ec009bc600701f94560bcf
ElonObey <gulin1999@mail.ru>
2022-03-30 22:05:02 +0300

    chore[frontend]: minor changes to qa component

--- frontend/src/components/QA/QA.jsx
+++ frontend/src/components/QA/QA.jsx
@@ -1,47 +1,54 @@
+import React, { useState, useContext } from 'react';
 import styles from './QA.module.scss';
 import plus from '../../assets/images/Plus.svg';
 
 const QA = () => {
+    const [active, setActive] = useState(false);
+
+    const toggleClass = () => {
+        setActive(!active);
+    }
+
     return (
-        <div className={styles.content}>
-            <div className={styles.block}>
-                <div className={styles.name}>
-                    <a> Lorem Ipsum</a>
-                </div>
-                <div className={styles.img}>
-                    <a href='#'><img src={plus} /></a>
-                </div>
-            </div>
+        <div className={[styles.content, styles.screen].join(' ')}>
+            <div className={styles.qa}>
 
-            <div className={styles.block}>
-                <div className={styles.name}>
-                    <a> Lorem Ipsum</a>
+                <div className={active ? [styles.question, styles.active].join(' ') : styles.question}>
+                    <div className={styles.question__header} onClick={toggleClass}>
+                        <span className={styles.name}>Lorem ipsum dolor sit amet, consectetur adipisicing elit?</span>
+                        <img src={plus} />
+                    </div>
+                    <div className={styles.question__body}>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Tenetur, architecto, explicabo perferendis nostrum,
+                            maxime impedit atque odit sunt pariatur illo obcaecati
+                            soluta molestias iure facere dolorum adipisci eum? Saepe,
+                            itaque.
+                        </p>
+                    </div>
                 </div>
-                <div className={styles.img}>
-                    <a href='#'><img src={plus} /></a>
-                </div>
-            </div>
 
-            <div className={styles.block}>
-                <div className={styles.name}>
-                    <a> Lorem Ipsum</a>
+                <div className={active ? [styles.question, styles.active].join(' ') : styles.question}>
+                    <div className={styles.question__header} onClick={toggleClass}>
+                        <span className={styles.name}>Lorem ipsum dolor sit amet, consectetur adipisicing elit?</span>
+                        <img src={plus} />
+                    </div>
+                    <div className={styles.question__body}>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Tenetur, architecto, explicabo perferendis nostrum,
+                            maxime impedit atque odit sunt pariatur illo obcaecati
+                            soluta molestias iure facere dolorum adipisci eum? Saepe,
+                            itaque.
+                        </p>
+                    </div>
                 </div>
-                <div className={styles.img}>
-                    <a href='#'><img src={plus} /></a>
-                </div>
+
             </div>
-            
-            <div className={styles.block}>
-                <div className={styles.name}>
-                    <a> Lorem Ipsum</a>
-                </div>
-                <div className={styles.img}>
-                    <a href='#'><img src={plus} /></a>
-                </div>
-            </div>
-            
         </div>
     );
 };
 
 export { QA };
+
