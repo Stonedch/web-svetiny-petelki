72a6d05423a619fc2f2858e36d3c7f0cc2636155
Maxim Mukhin <stch.max.muhin@gmail.com>
2022-04-01 01:02:49 +0300

    chore[frontend]" categories from backend

--- frontend/src/components/Categories/Categories.jsx
+++ frontend/src/components/Categories/Categories.jsx
@@ -1,18 +1,24 @@
+import React, { useState, useEffect } from 'react';
 import styles from './Categories.module.scss';
 import image from '../../assets/images/header-image.png';
 import { Category } from '../Category';
 
 const Categories = () => {
-    return (
+    const [ data, setData ] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost/api/v1/categories/')
+            .then(response => response.json())
+            .then(response => setData(response.results));
+    }, []);
+
+    const categories = (
         <div className={[styles.categories, styles.content, styles.screen].join(' ')}>
-            <Category title="Именные игрушки" picture={image} />
-            <Category title="Именные игрушки" picture={image} />
-            <Category title="Именные игрушки" picture={image} />
-            <Category title="Именные игрушки" picture={image} />
-            <Category title="Именные игрушки" picture={image} />
-            <Category title="Именные игрушки" picture={image} />
+            {data.map(category => <Category title={category.name} picture={category.picture} />)}
         </div>
     );
+
+    return data ? categories : null;
 }
 
 export { Categories };
