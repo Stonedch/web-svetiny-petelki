38157f3b4249b32aa3f4f461d5734305491288ad
Maxim Mukhin <stch.max.muhin@gmail.com>
2022-04-01 01:22:23 +0300

    fix[frontend]: change api url

--- frontend/src/components/Categories/Categories.jsx
+++ frontend/src/components/Categories/Categories.jsx
@@ -1,24 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import styles from './Categories.module.scss';
-import image from '../../assets/images/header-image.png';
 import { Category } from '../Category';
+import { API_URL } from '../../http';
 
 const Categories = () => {
     const [ data, setData ] = useState(null);
 
     useEffect(() => {
-        fetch('http://localhost/api/v1/categories/')
+        fetch(API_URL + 'categories/')
             .then(response => response.json())
             .then(response => setData(response.results));
     }, []);
 
-    const categories = (
+    return data ? (
         <div className={[styles.categories, styles.content, styles.screen].join(' ')}>
             {data.map(category => <Category title={category.name} picture={category.picture} />)}
         </div>
-    );
-
-    return data ? categories : null;
+    ) : null;
 }
 
 export { Categories };
