9ea0f647d26b9b1d31d2f033e44662ba7d1b4df7
ElonObey <gulin1999@mail.ru>
2022-04-02 20:47:05 +0300

    chore[frontend]: questions from backend

--- frontend/src/components/Questions/Questions.jsx
+++ frontend/src/components/Questions/Questions.jsx
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { QA } from '../Question';
 import styles from './Questions.module.scss';
+import { API_URL } from '../../http';
 
 const Questions = () => {
+    const [ data, setData] = useState(null);
 
-    return (
+    useEffect(() => {
+        fetch(API_URL + 'questions/')
+            .then(response => response.json())
+            .then(response => setData(response.results));
+    }, []);
+
+    return data ? (
         <div className={[styles.components, styles.content, styles.screen].join(' ')}>
-            <QA title='Lorem ipsum dolor sit amet'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </QA>
-            <QA title='Lorem ipsum dolor sit amet'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </QA>
+            {data.map(question => <QA title={question.title}>{question.body}</QA>)}
         </div>
-    );
+    ) : null;
 }
 
 export { Questions };
