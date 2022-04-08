2e8128308598dac220a854feb48bea32957303a1
Maxim Mukhin <stch.max.muhin@gmail.com>
2022-04-08 16:45:54 +0300

    chore[frontend]: add links to products

--- frontend/src/components/Category/Category.jsx
+++ frontend/src/components/Category/Category.jsx
@@ -1,15 +1,16 @@
 import styles from './Category.module.scss';
+import { Link } from 'react-router-dom';
 
 const Category = (props) => {
     const {title, picture} = props
 
     return (
-        <div className={styles.category}>
+        <Link className={styles.category} to='/products/'>
             <img src={picture} />
             <div className={styles.category__content}>
                 <h2 className={styles.category__title}>{title}</h2>
             </div>
-        </div>
+        </Link>
     );
 }
 
