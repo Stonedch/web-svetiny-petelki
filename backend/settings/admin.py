7a4e776a2a6f87bdd9c07adc2e9db04582e0cf59
Maxim Mukhin <stch.max.muhin@gmail.com>
2022-04-11 16:03:11 +0300

    chore[backend]: add settings/link to django-admin

--- backend/settings/admin.py
+++ backend/settings/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Settings, Social
+from .models import Settings, Social, Link
 
 admin.site.register(Settings)
+admin.site.register(Link)
 admin.site.register(Social)
 
