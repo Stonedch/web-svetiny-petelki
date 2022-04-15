29c41fec7067253a35fb70c8125057c01a0e2595
Maxim Mukhin <stch.max.muhin@gmail.com>
2022-04-15 16:29:32 +0300

    feat[backend]: add filter by category id to products

--- backend/web_svetpet/settings.py
+++ backend/web_svetpet/settings.py
@@ -97,6 +97,9 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
     'PAGE_SIZE': 100,
 }
 
