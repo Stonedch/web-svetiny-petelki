622f5128bbd595e5882030a628eb7fad381bab7c
Maxim Mukhin <stch.max.muhin@gmail.com>
2022-05-03 17:53:38 +0300

    feat[frontend]: update the Header component

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,35 +1,40 @@
-import styles from './Header.module.scss';
-import image from 'assets/images/header-image.png';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Header.module.scss';
+import logotype from 'assets/images/logo.svg';
+import { Navbar } from './Navbar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faVk, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faBars, faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
-  return (
-    <header className={styles.header}>
-      <div className={styles.content}>
-        <div className={styles.wrapper}>
-          <div className={styles.image}>
-            <img src={image} />
-          </div>
-        </div>
-        <div className={styles.wrapper}>
-          <h1>Lorem Ipsum</h1>
-          <p className={styles.header__par}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-          <p className={styles.header__par}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
-        </div>
-      </div>
-    </header>
-  );
+    return (
+        <header className={[styles.header, styles.screen].join(' ')}>
+            <div className={styles.content}>
+                <div className={styles.top}>
+                    <div className={styles.socials}>
+                        <a href='#'>
+                            <FontAwesomeIcon icon={faVk} />
+                        </a>
+                        <a href='#'>
+                            <FontAwesomeIcon icon={faInstagram} />
+                        </a>
+                    </div>
+                    <Link className={styles.brand} to='/home/'>
+                        <div className={styles.logotype}>
+                            <img src={logotype} />
+                        </div>
+                        <h1 className={styles.title}>Светины Петельки</h1>
+                    </Link>
+                    <div className={styles.tools}>
+                        <FontAwesomeIcon icon={faUser} />
+                        <FontAwesomeIcon icon={faBars} />
+                    </div>
+                </div>
+                <Navbar />
+            </div>
+        </header>
+    );
 };
 
 export { Header };
--- frontend/src/components/Header/Navbar/Navbar.jsx
+++ frontend/src/components/Header/Navbar/Navbar.jsx
@@ -1,79 +1,24 @@
-import React, { useState, useContext, useEffect } from 'react';
-import { API_URL } from 'http';
-import { Link } from 'react-router-dom';
-import { Context } from 'index';
-import { ModalWindow } from 'components/ModalWindow';
-import { AuthForm } from 'components/AuthForm';
+import React, { useState, useEffect } from 'react';
 import styles from './Navbar.module.scss';
-import img from 'assets/images/Logo.svg';
-import facebook from 'assets/images/Facebook.svg';
-import instag from 'assets/images/Instagram.svg';
-import whatsapp from 'assets/images/WhatsApp.svg';
-import phone from 'assets/images/Phone.svg';
+import { Link } from 'react-router-dom';
+import { API_URL } from 'http';
 
 const Navbar = () => {
-    const [modalActive, setModalActive] = useState();
-    const [ data, setData ] = useState(null);
-    const { store } = useContext(Context);
-    console.warn(store.isAuth);
+    const [navbar, setNavbar] = useState(null);
 
     useEffect(() => {
         fetch(API_URL + 'settings/navbar/')
-            .then(response => response.json())
-            .then(response => setData(response.results));
+            .then((response) => response.json())
+            .then((response) => setNavbar(response.results));
     }, []);
 
-    const login = (
-        <span className={styles.auth} onClick={() => setModalActive(true)}>
-            Войти
-        </span>
-    );
-
-    const logout = (
-        <span className={styles.auth} onClick={() => { store.logout(); window.location.reload() }}>
-            Выйти
-        </span>
-    );
-
-    const navbar = data ? (
-        <>
-            {data.map((item)=> <Link to={item.url}>{item.name}</Link>)}
-        </>
-    ) : null;
-
-    return (
-        <div className={styles.component}>
-            <div className={styles.content}>
-                <div className={styles.section}>
-                    <a href="/" className={styles.logo}>
-                        <img src={img} />
-                    </a>
-                    <div className={styles.menu}>
-                        {navbar}
-                    </div>
-                </div>
-                <div className={styles.section}>
-                    <div className={styles.soc}>
-                        <a href="#"><img src={facebook} /></a>
-                        <a href="#"><img src={instag} /></a>
-                        <a href="#"><img src={whatsapp} /></a>
-                    </div>
-                    <div className={styles.phone}>
-                        <img src={phone} />
-                        <a href="tel: +70000000000"> +7 (000) 000-00-00 </a>
-                    </div>
-                    {localStorage.access ? logout : login}
-                    <div className={styles.hamburger}>
-                        <span></span>
-                    </div>
-                </div>
-            </div>
-            <ModalWindow active={modalActive} setActive={setModalActive}>
-                <AuthForm />
-            </ModalWindow>
+    return navbar ? (
+        <div className={styles.navbar}>
+            {navbar.map((item) => (
+                <Link to={item.url}>{item.name}</Link>
+            ))}
         </div>
-    );
-}
+    ) : null;
+};
 
 export { Navbar };
-
