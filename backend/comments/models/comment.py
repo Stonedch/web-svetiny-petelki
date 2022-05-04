15a53c602d5000570b575a1c4e07611bcbc6ac29
Maxim Mukhin <stch.max.muhin@gmail.com>
2022-05-04 16:46:29 +0300

    feat[backend]: translate comments to ru

--- backend/comments/models/comment.py
+++ backend/comments/models/comment.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 from users.models import User
 from products.models import Product
@@ -10,20 +11,26 @@ class Comment(models.Model):
         related_name='comments',
         related_query_name='comment',
         on_delete=models.CASCADE,
-        blank=False, null=False
+        blank=False, null=False,
+        verbose_name=_('author')
     )
     product = models.ForeignKey(
         Product,
         related_name='products',
         related_query_name='product',
         on_delete=models.SET_NULL,
-        blank=True, null=True
+        blank=True, null=True,
+        verbose_name=_('product')
     )
-    rating = models.IntegerField(blank=True, null=True)
-    body = models.TextField(max_length=512)
-    active = models.BooleanField(default=False, blank=False, null=False)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    rating = models.IntegerField(blank=True, null=True, verbose_name=_('rating'))
+    body = models.TextField(max_length=512, verbose_name=_('body'))
+    active = models.BooleanField(default=False, blank=False, null=False, verbose_name=_('active'))
+    created = models.DateTimeField(auto_now_add=True, verbose_name=_('created'))
+    updated = models.DateTimeField(auto_now=True, verbose_name=_('updated'))
+
+    class Meta:
+        verbose_name = _('comment')
+        verbose_name_plural = _('comments')
 
     def __str__(self):
         return f"{self.author}, {self.created}"
