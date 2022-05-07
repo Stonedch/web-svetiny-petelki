8755c80ad3d96e951deae11a8db8b56829921e39
Maxim Mukhin <stch.max.muhin@gmail.com>
2022-05-07 14:38:46 +0300

    chore[frontend]: tailwind header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,30 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
-import logotype from 'assets/images/logo.svg';
+import { Brand } from './Brand';
 import { Navbar } from './Navbar';
 import { Socials } from './Socials';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
     return (
-        <header className={[styles.header, styles.screen].join(' ')}>
+        <header className={styles.header}>
             <div className={styles.content}>
-                <div className={styles.top}>
-                    <Socials />
-                    <Link className={styles.brand} to='/home/'>
-                        <div className={styles.logotype}>
-                            <img src={logotype} />
-                        </div>
-                        <h1 className={styles.title}>Светины Петельки</h1>
-                    </Link>
-                    <div className={styles.tools}>
-                        <FontAwesomeIcon icon={faUser} />
-                        <FontAwesomeIcon icon={faBars} />
-                    </div>
-                </div>
+                <Brand />
                 <Navbar />
+                <Socials />
             </div>
         </header>
     );
